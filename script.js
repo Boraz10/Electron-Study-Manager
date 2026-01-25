408d2e5ef8dc1c4c607aa782a80cd2017ceeb710
Boraz10 <austinborazio@gmail.com>
2026-01-26 02:59:26 +1100

    Slightly refactored for Typescript

--- script.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById('bodyButton');
-const textArea = document.getElementById('textArea');
-
-console.log("Script loaded.");
-
-// Push button
-button.addEventListener('click', () => {
-    textArea.textContent = "Button Clicked!";
-});
